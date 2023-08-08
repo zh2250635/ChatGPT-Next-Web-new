d90c20578fad9597f66661816c68a6a6306d1782
zh2250635 <116276983+zh2250635@users.noreply.github.com>
2023-08-08 01:26:43 -0700

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -50,7 +50,7 @@ export const REQUEST_TIMEOUT_MS = 60000;
 export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
 export const OpenaiPath = {
-  ChatPath: "v1/chat/completions",
+  ChatPath: "",
   UsagePath: "dashboard/billing/usage",
   SubsPath: "dashboard/billing/subscription",
   ListModelPath: "v1/models",
